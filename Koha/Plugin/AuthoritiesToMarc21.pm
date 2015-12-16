88d6b5c0650e3468867aff62489cb047ff581a9a
kanellov <contact@kanellov.com>
2015-12-16 23:41:05 +0200

    fix uploading marc file; change response content-type

--- Koha/Plugin/AuthoritiesToMarc21.pm
+++ Koha/Plugin/AuthoritiesToMarc21.pm
@@ -7,7 +7,7 @@ use Modern::Perl;
 use base qw(Koha::Plugins::Base);
 
 ## We will also need to include any Koha libraries we want to access
-use Data::Dump qw(dump);
+use Data::Dumper;
 
 use Koha::Upload;
 use MARC::Moose::Formater::AuthorityUnimarcToMarc21;
@@ -33,18 +33,12 @@ our $metadata = {
 ## More can be added, but none should be removed
 sub new {
     my ( $class, $args ) = @_;
-
-    ## We need to add our metadata here so our base class can access it
     $args->{'metadata'} = $metadata;
     $args->{'metadata'}->{'class'} = $class;
-
-    ## Here, we call the 'new' method for our base class
-    ## This runs some additional magic and checking
-    ## and returns our actual $self
-    my $self = $class->SUPER::new($args);
-
-    return $self;
+    return $class->SUPER::new($args);
 }
+
+sub install { return 1; }
 
 sub uninstall { return 1; }
 
@@ -52,49 +46,51 @@ sub tool {
     my ( $self, $args ) = @_;
     my $cgi = $self->{'cgi'};
 
-    unless ( $cgi->param('submitted') ) {
-        $self->tool_step1();
+    if ( $cgi->param('submitted') ) {
+        $self->download_file();
     } else {
-       $self->tool_step2();
+        $self->tool_form();
     }
-
 }
 
-sub tool_step1 {
+sub tool_form {
     my ( $self, $args ) = @_;
     my $cgi = $self->{'cgi'};
-    my $template = $self->get_template({ file => 'tool-step1.tt' });
+    my $template = $self->get_template({ file => 'tool-form.tt' });
 
     print $cgi->header();
     print $template->output();
 }
 
-sub tool_step2 {
+sub download_file {
     my ( $self, $args ) = @_;
     my $cgi = $self->{'cgi'};
-    my $template = $self->get_template({ file => 'tool-step2.tt' });
-
     my $fileID = $cgi->param('uploadedfileid');
-
     if($fileID) {
         my $upload = Koha::Upload->new->get({ id => $fileID, filehandle => 1 });
         my $fh = $upload->{fh};
         my $filename = $upload->{name}; # filename only, no path
         my $marcrecord= '';
-        $/ = "\035";
+        local $/ = "\035";
         while (<$fh>) {
             s/^\s+//;
             s/\s+$//;
             $marcrecord.=$_;
         }
         $fh->close;
+
+        my $iso2709Parser = MARC::Moose::Parser::Iso2709->new;
         my $toMarc21Formater = MARC::Moose::Formater::AuthorityUnimarcToMarc21->new();
         my $marc21 = $toMarc21Formater->format($iso2709Parser->parse($marcrecord));
-        # should download unimarc file
-    }
 
-    print $cgi->header();
-    print $template->output();
+        print $cgi->header(
+            -type => 'application/octet-stream',
+            -attachment => 'converted.marc',
+        );
+        print $marc21->as('Text');
+        return 1;
+    }
+    return 0;
 }
 
 1;
